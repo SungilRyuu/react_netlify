9c6fb0a0df6f05394f6d28a8d6e65aeaec0e0c18
sungilRyuu <x7nxc3@gmail.com>
2022-04-22 12:02:17 +0900

    0422

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,34 +1,31 @@
 import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
-import Title from "../layout/Title";
-import ReferCont from "../includes/ReferCont";
 import Footer from "../layout/Footer";
+import Title from "../layout/Title";
+import Contact from "../layout/Contact";
 import Loading from "../basics/Loading";
+import ReferCont from "../includes/ReferCont";
 import {gsap} from "gsap";
 import axios from "axios";
-
-
-
 
 class Reference extends React.Component {
     state = {
         isLoading: true,
-        refers: []
+        refers: [],
     }
-
-    getSite = () => {
+    mainAnimation = () => {
         setTimeout(() => {
             gsap.to("#header", {
-              duration:0.8, 
-              top: 0, 
-              ease: "sine.out"
+                duration: 0.8,
+                top: 0,
+                ease: "sine.out"
             });
             gsap.to("#footer", {
-              duration:0.8, 
-              bottom: 0,
-              ease: "sine.out",
-              delay: 0.3
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.2,
+                ease: "sine.out"
             });
             gsap.to(".cont__title h1", {
                 duration: 1,
@@ -38,79 +35,97 @@ class Reference extends React.Component {
                 delay: 1.0,
                 ease: "back.out"
             })
-            gsap.to(".refer__inner h2", {
-                duration: 1,
-                x: 0,
+            gsap.to(".cont__title strong", {
+                duration: 0.7,
                 y: 0,
+                x: 0,
                 opacity: 1,
                 delay: 1.0,
-                ease: "back.out"
-            })
-            gsap.to(".refer__inner table", {
-              duration:1, 
-              x: 0,
-              opacity: 1, 
-              delay: 1.5,
-              ease:"back.out(1.7)"
+                ease: "power4.out"
             });
-        },)
+            gsap.to(".cont__title em", {
+                duration: 0.7,
+                y: 0,
+                x: 0,
+                opacity: 1,
+                delay: 1.3,
+                ease: "power4.out"
+            });
+            // gsap.to(".refer__inner", {
+            //     duration: 0.5,
+            //     y: 0,
+            //     opacity: 1,
+            //     delay: 1.6,
+            //     ease: "power3.out"
+            // });
+        }, 10)
     }
-
-    getAbout = async () => {
-        const {data: {data: {refer}}} = await axios.get("https://sungilryuu.github.io/react/react2022/src/assets/json/reference.json");
-
-        this.setState({refers: refer})
-
-        setTimeout(() => {
-            console.log("두 번째 시작");
-            this.setState({isLoading: false, refers:refer});
-            this.getSite();
-        }, 1000)
+    getRefers = async () => {
+        const {
+            data: {
+                data: {htmlRefer},
+            },
+        } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
+        this.setState({ refers: htmlRefer,  isLoading: false})
+        this.mainAnimation();
     }
 
     componentDidMount(){
         setTimeout(() => {
-            console.log("첫 번째 시작");
-            document.getElementById("loading").classList.remove("loading__active")
-            document.querySelector("body").style.background = "var(--light_bg)"
-            this.getAbout();
-        },2000)
+            document.getElementById("loading").classList.remove("loading__active");
+            this.getRefers();
+        }, 2000);
     }
 
     render(){
         const {isLoading, refers} = this.state;
-
         console.log(refers)
         return (
             <>
                 {isLoading ? (
-                <Loading color="light" />
-                    ):(<>
-                        <Header color="light"/>
+                    <Loading color="black" />
+                ) : (
+                    <>
+                        <Header />
                         <Contents>
-                            <Title title={[ "REFERENCE", "book"]} color="light" />
-                            <ReferCont color="light" refer={refers} />
+                            <Title title={["Reference", "Book"]} />
+                            <section className="refer__cont">
+                                <div className="container">
+                                    <div className="refer__inner">
+                                        <h2>CSS</h2>
+                                        <ul className="refer__list">
+                                            {refers.map(refer => (
+                                                <ReferCont 
+                                                    key={refer.id}
+                                                    id={refer.id}
+                                                    title={refer.title}
+                                                    desc={refer.desc}
+                                                    use={refer.use}
+                                                    desc2={refer.desc2}
+                                                    element={refer.element}
+                                                    tag={refer.tag}
+                                                    version={refer.version}
+                                                    view={refer.view}
+                                                    image={refer.image}
+                                                    link={refer.link}
+                                                    Definition={refer.Definition}
+                                                    Accessibility={refer.Accessibility}
+                                                    Related={refer.Related}
+                                                    mdn={refer.mdn}
+                                                    w3c={refer.w3c}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </section>
+                            <Contact />
                         </Contents>
-                        <Footer  color="light"/>
+                        <Footer />
                     </>
-                    )
-                }
+                )}
             </>
         )
     }
 }
-
-// function Reference(){
-//     return (
-//         <>
-//             <Header color="light"/>
-//             <Contents>
-//                 <Title title={[ "REFERENCE", "book"]} color="light" />
-//                 <ReferCont color="light" />
-//             </Contents>
-//             <Footer  color="light"/>
-//         </>
-//     )
-// }
-
 export default Reference;
